39f87a6c1040e211746c554704aaafa88a4229fe
wanjiku <elidakirigo@gmail.com>
2020-07-23 15:38:45 +0300

    topic6 - es6 features

---
+++ ts/IIFEs-ES6-functions.js
@@ -0,0 +1,16 @@
+/**
+ * --------------------------
+ * es6 features
+ * --------------------------
+ */
+/**
+ * some of the es6 features used in typescript include
+ * let,const
+ */
+var mycode = "using es6 features";
+var myLine = 2;
+/**
+ * ----------------
+ * the end
+ * ---------------
+ */ 
---
+++ ts/IIFEs-ES6-functions.ts
@@ -0,0 +1,18 @@
+/**
+ * --------------------------
+ * es6 features
+ * --------------------------
+ */
+/**
+ * some of the es6 features used in typescript include 
+ * let,const
+ */
+let mycode : string = "using es6 features"
+
+const myLine : number = 2;
+
+/**
+ * ----------------
+ * the end
+ * ---------------
+ */
