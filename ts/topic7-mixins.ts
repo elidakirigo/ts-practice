2d1566115ff5aa61e8e4da1a3deb764a3f8a4883
wanjiku <elidakirigo@gmail.com>
2020-07-24 19:33:51 +0300

    topic7 - mixins

---
+++ ts/topic7-mixins.ts
@@ -0,0 +1,30 @@
+class Flies {
+    fly() {
+    alert('Is it a bird? Is it a plane?');
+    }
+   }
+   class Climbs {
+    climb() {
+    alert('My spider-sense is tingling.');
+    }
+   }
+   class Bulletproof {
+    deflect() {
+    alert('My wings are a shield of steel.');
+    }
+   }
+
+   class BeetleGuy implements Climbs, Bulletproof {
+    climb: () => void;
+    deflect: () => void;
+   }
+
+   function applyMixins(derivedCtor: any, baseCtors: any[]) {
+    baseCtors.forEach(baseCtor => {
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
+    if (name !== 'constructor') {
+    derivedCtor.prototype[name] = baseCtor.prototype[name];
+    }
+    });
+    });
+   }
